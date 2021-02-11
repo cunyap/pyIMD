7e32164571f879c858bd236b48443d53768a4a95
cunyap <acuny@bluewin.ch>
2021-02-11 10:29:15 +0100

    Update example_usage_script.py

--- pyIMD/examples/example_usage_script.py
+++ pyIMD/examples/example_usage_script.py
@@ -45,4 +45,4 @@ imd.load_pyimd_project("/pyIMD/examples/data/show_case/pyIMDProjectName.xml")
 # change a parameter i.e
 imd.settings.figure_format = 'png'
 # and run again
-imd.run_intertial_mass_determination()
+imd.run_inertial_mass_determination()
